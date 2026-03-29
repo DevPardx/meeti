37a0fdcdb34b57ae6670cf810b95f2659dc92254
DevPardx <engineer.pardo@gmail.com>
2026-03-28 20:26:20 -0600

    Add layout and header components, update ESLint rules, and modify package scripts

---
+++ src/shared/components/ui/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Logo() {
+    return (
+        <Image
+            src="/logo.svg"
+            alt="Meeti Logo"
+            width={300}
+            height={150}
+        />
+    );
+}
